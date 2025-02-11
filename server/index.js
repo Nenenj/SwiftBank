1fea4ecc73457b10e65ed52f327ae590dedcaa76
nenenj <nnennanjoku08@gmail.com>
2025-02-11 16:37:42 +0100

    Fix port binding for Render

--- server/index.js
+++ server/index.js
@@ -39,8 +39,8 @@ sequelize.authenticate()
     console.log('Database & tables synchronized! ');
 
     // Start the server
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server is running on phttp://0.0.0.0:${port}`);
     });
   })
   .catch((err) => {
